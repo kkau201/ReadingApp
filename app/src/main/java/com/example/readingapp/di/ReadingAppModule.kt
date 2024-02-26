7b6540f313f8518b111bfe80efdc2ce0200eb3ca
Kayla <kaylak106@hotmail.co.nz>
2024-02-27 08:51:00 +1300

    Add book api

--- app/src/main/java/com/example/readingapp/di/ReadingAppModule.kt
+++ app/src/main/java/com/example/readingapp/di/ReadingAppModule.kt
@@ -1,11 +1,27 @@
 package com.example.readingapp.di
 
+import com.example.readingapp.network.BookApi
+import com.example.readingapp.utils.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ReadingAppModule {
 
+    @Singleton
+    @Provides
+    fun provideBookApi(): BookApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create()
+    }
 }
---
+++ app/src/main/java/com/example/readingapp/network/BookApi.kt
@@ -0,0 +1,18 @@
+package com.example.readingapp.network
+
+import com.example.readingapp.model.Book
+import com.example.readingapp.model.Item
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface BookApi {
+    @GET("volumes")
+    suspend fun getAllBooks(@Query("q") query: String): Book
+
+    @GET("volumes/{bookId}")
+    suspend fun getBookById(@Path("bookId") bookId: String): Item
+
+}
