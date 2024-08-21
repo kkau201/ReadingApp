541d398354184e56f75033cb8f000647d3d6ebb4
Kayla <kaylak106@hotmail.co.nz>
2024-08-22 09:43:33 +1200

    Add scaffold to UserScreen

--- app/src/main/java/com/example/readingapp/ui/user/UserScreen.kt
+++ app/src/main/java/com/example/readingapp/ui/user/UserScreen.kt
@@ -2,6 +2,8 @@ package com.example.readingapp.ui.user
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -10,12 +12,14 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
@@ -28,13 +32,14 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import coil.compose.AsyncImage
 import com.example.readingapp.R
 import com.example.readingapp.common.ViewModelBinding
 import com.example.readingapp.common.observeLifecycle
+import com.example.readingapp.ui.components.ReadingAppBarNav
 import com.example.readingapp.ui.components.ReadingAppButton
-import com.example.readingapp.ui.details.ObserveDetailsLifecycleEvents
 import com.example.readingapp.ui.theme.AppTheme
+import com.example.readingapp.ui.theme.AppTheme.spacing
+import com.example.readingapp.ui.theme.Purple
 import com.example.readingapp.ui.user.update.Avatar
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -51,17 +56,30 @@ fun UserScreen(
 
     ObserveUserLifecycleEvents(lifecycleOwner, viewModel)
 
-    UserContent(
-        userDisplayName = uiState.user?.displayName ?: "",
-        userBio = uiState.user?.quote ?: "",
-        userAvatar = uiState.user?.avatar,
-        viewModel::navigateToUpdateUser,
-        viewModel::onLogoutClick
-    )
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = AppTheme.colors.background,
+        topBar = {
+            ReadingAppBarNav(
+                navIconTint = Color.Black,
+                onNavIconClick = viewModel::navigateBack
+            )
+        }
+    ) { padding ->
+        UserContent(
+            modifier = Modifier.padding(padding),
+            userDisplayName = uiState.user?.displayName ?: "",
+            userBio = uiState.user?.quote ?: "",
+            userAvatar = uiState.user?.avatar,
+            viewModel::navigateToUpdateUser,
+            viewModel::onLogoutClick,
+        )
+    }
 }
 
 @Composable
 fun UserContent(
+    modifier: Modifier = Modifier,
     userDisplayName: String,
     userBio: String,
     userAvatar: Avatar? = null,
@@ -70,14 +88,16 @@ fun UserContent(
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.padding(vertical = AppTheme.spacing.lgSpacing, horizontal = AppTheme.spacing.mdSpacing)
+        modifier = modifier.padding(vertical = spacing.lgSpacing, horizontal = spacing.mdSpacing)
     ) {
         userAvatar?.let {
             Image(
                 painter = painterResource(id = userAvatar.img),
                 contentDescription = stringResource(R.string.cont_desc_profile_image),
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.clip(CircleShape).size(200.dp)
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(200.dp)
             )
         } ?: run {
             Icon(
@@ -90,21 +110,25 @@ fun UserContent(
             text = userDisplayName,
             fontWeight = FontWeight.Bold,
             style = AppTheme.typography.titleLarge,
-            modifier = Modifier.padding(top = AppTheme.spacing.mdSpacing)
+            modifier = Modifier.padding(top = spacing.mdSpacing)
         )
         Text(
             text = userBio,
             style = AppTheme.typography.bodyLarge,
             textAlign = TextAlign.Center,
-            modifier = Modifier.padding(top = AppTheme.spacing.smSpacing, bottom = AppTheme.spacing.lgSpacing)
+            modifier = Modifier.padding(top = spacing.smSpacing, bottom = spacing.lgSpacing)
         )
         ReadingAppButton(
             text = "Update Details",
+            padding = PaddingValues(horizontal = spacing.mdSpacing),
+            backgroundColor = Purple,
             onClick = onUpdateClick,
             modifier = Modifier.fillMaxWidth()
         )
         ReadingAppButton(
             text = "Logout",
+            padding = PaddingValues(horizontal = spacing.mdSpacing),
+            backgroundColor = Purple,
             onClick = onLogoutClick,
             modifier = Modifier.fillMaxWidth()
         )
